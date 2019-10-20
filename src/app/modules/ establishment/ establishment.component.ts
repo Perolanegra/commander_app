d7d7becf07ee4ab82a7ae204e494e7b054e5035c
Perolanegra <pedratto3@gmail.com>
2019-10-20 18:39:13 -0300

    Remocao de codigo inutilizado

--- src/app/modules/ establishment/ establishment.component.ts
+++ src/app/modules/ establishment/ establishment.component.ts
@@ -20,8 +20,7 @@ export class EstablishmentComponent implements OnInit {
     private googleService: GoogleService,
     private distanceService: DistanceService) { }
 
-    async ngOnInit() {
-        
+    ngOnInit() {
         this.getBars();
     }
 
@@ -46,19 +45,5 @@ export class EstablishmentComponent implements OnInit {
         });
     }
 
-    calculate() {
-        var myLatLng1 = { lat: 40.634315, lng: 14.602552 };
-        var myLatLng2 = { lat: 40.04215, lng: 14.102552 };
-    }
-
-    getEstablishments(lat1:number,lat2:number,long1:number,long2:number){
-        let p = 0.017453292519943295;    // Math.PI / 180
-        let c = Math.cos;
-        let a = 0.5 - c((lat1-lat2) * p) / 2 + c(lat2 * p) *c((lat1) * p) * (1 - c(((long1- long2) * p))) / 2;
-        let dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
-        return dis;
-    }
-
-      
 
 }
