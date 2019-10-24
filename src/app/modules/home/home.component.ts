da40549766d2d61e005949bd53cb0713ba1b12ed
Perolanegra <pedratto3@gmail.com>
2019-10-23 23:49:50 -0300

    Criado Component SuperTabs

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -13,56 +13,22 @@ import { EstablishmentDetComponent } from '../establishment/establishment-det/es
   styleUrls: ['home.component.scss'],
 })
 export class HomeComponent {
-  // public tabEstablishment = EstablishmentComponent;
+  public tabEstablishment = EstablishmentComponent;
   @ViewChild(SuperTabs, { static: false }) superTabs: SuperTabs;
-  bars: any;
 
   constructor(public appController: AppController,
   private establishmentService: EstablishmentService,
   private modalCtrl: ModalController,
   private googleService: GoogleService) { }
 
+  private switchVar: string = 'teste1';
+
   async ngAfterViewInit() {
-    this.superTabs.selectTab(0);
+    // this.superTabs.selectTab(0);
   }
 
-  async ngOnInit() {
-    await this.getBars();
-  }
-
-  async getBars() { // mudar essa lógica
-    // lista dos cadastrados
-    const loader = await this.appController.presentLoadingDefault();
-    this.bars = await this.establishmentService.getAll();
-    // pesquisar os bares com base no nearBy e trazer os cadastrados
-    // depois fazer o foreach abaixo
-    // hoje estou trazendo todos, porém estático no service pq ainda n fiz a implementacao do front com o back e nem cadastrei os bares no banco.
-
-    this.bars.forEach(async (bar) => {
-      this.googleService.getDistance(bar.lat, bar.lng).then(resp => {
-        bar.distance = resp['distance'].toFixed(1);
-        bar.duration = resp['duration'];
-      });
-    });
-
-    loader.dismiss();
-
-    // obtenho a lista dos restaurantes proximos
-    // const nearBy = await this.establishmentService.getEstablishmentsNearBy();
-    // console.log('nearBy: ', nearBy);
-  }
-
-  async pushToDetails(bar) {
-    const modal = await this.modalCtrl.create({
-      component: EstablishmentDetComponent,
-      componentProps: {
-        "bar": bar
-      },
-    });
-
-    await modal.present();
-
-    modal.onWillDismiss().then(() => this.getBars());
+  handleSwitch(newSwitch: string) {
+    this.switchVar = newSwitch;
   }
 
 }
