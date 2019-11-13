95c05ec0e8342300a12eec0a30d12059824c2477
Perolanegra <pedratto3@gmail.com>
2019-11-13 12:00:10 -0300

    Instalado datePicker

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppController } from '../core/appController';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +8,23 @@ import { AppController } from '../core/appController';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  forms: FormGroup;
 
-  constructor(public appController: AppController) { }
+  constructor(public appController: AppController,
+  private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-
+    this.forms = this.createForm();
   }
 
+  createForm(): FormGroup {
+    return this.formBuilder.group({
+      name: ["", Validators.required],
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+      birthDate: [""],
+      phone: [""],
+    });
+  }
 
 }
